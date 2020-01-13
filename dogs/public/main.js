e149ff0d29d3e022902864dee90ff694bd0f7ab6
abhishek <abhishek.goswami@pipedrive.com>
2020-01-13 02:19:01 +0200

    3. fetch dog images and display

--- dogs/public/main.js
+++ dogs/public/main.js
@@ -1,3 +1,12 @@
-;(function (d, w) {
+;(async function (d, w) {
+    const { message: dogImages } = await fetch('https://dog.ceo/api/breeds/image/random/7').then(d => d.json());
 
+    const html =dogImages.reduce((a, c) => {
+        a += `<div class='dog'><img src='${c}' /></div>`;
+
+        return a;
+    }, '');
+
+    const app = d.getElementById('app');
+    app.innerHTML = html;
 })(document, window);
