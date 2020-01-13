0c0ecc9679a4f0323f7e05e7a564249145aeecd8
abhishek <abhishek.goswami@pipedrive.com>
2020-01-13 16:21:41 +0200

    handle fetch network error aka offline mode

--- dogs/public/sw.js
+++ dogs/public/sw.js
@@ -1,1 +1,18 @@
 const version = 1;
+
+self.addEventListener('fetch', (event) => {
+    const url = event.request.url;
+
+    event.respondWith(
+        fetch(event.request)
+        .catch(err => {
+            console.log('sw: error occurred while fetch', event.request.url);
+
+            return new Response('<b>You seem to be offline</b>', {
+                headers: {
+                    'Content-Type': 'text/html',
+                },
+            });
+        })
+    )
+});
