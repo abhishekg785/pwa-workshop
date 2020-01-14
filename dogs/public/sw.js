dc23a9a112b6a2912fa706323286dd33d2ae9269
abhishek <abhishek.goswami@pipedrive.com>
2020-01-14 02:59:26 +0200

    precache, delete unused cache

--- dogs/public/sw.js
+++ dogs/public/sw.js
@@ -1,22 +1,69 @@
-const version = 2;
+const version = 4;
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        handleRequest(event.request)
+const assetsToCache = [
+    '/',
+    '/main.js',
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        precache()
     )
 });
 
-async function handleRequest(request) {
-    try {
-        const response = await fetch(request);
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        cleanCache()
+    )
+});
 
-        const cache = await caches.open('dogs-pwa');
-        await cache.put(request, response.clone());
+self.addEventListener('fetch', (event) => {
+    console.log(`sw: fetch: ${event.request.url}`, event.request);
+    const url = new URL(event.request.url);
+
+    if ((url.hostname === location.hostname) && event.request.method === 'GET') {
+        event.respondWith(
+            handleSameOriginRequest(event.request)
+        )
+    }
+});
+
+async function precache() {
+    console.log('sw: precache: caching static assets');
+
+    const cache = await caches.open(`dogs-pwa-static-assets-${version}`);
+    await cache.addAll(assetsToCache);
+    console.log('sw: precache: static assets  cached');
+}
+
+async function cleanCache() {
+    console.log('sw: cleanCache: cleaning unused cache');
+    const cacheKeys = await caches.keys();
+
+    await Promise.all(
+        cacheKeys.filter(cacheName => {
+            if (/dogs-pwa/.test(cacheName)) {
+                if (cacheName !== `dogs-pwa-static-assets-${version}`) {
+                    return caches.delete(cacheName);
+                }
+            }
+        })
+    )
+    console.log('sw: cleanCache: unused cache removed');
+}
+
+// cache only
+async function handleSameOriginRequest(request) {
+    const requestUrl = request.url;
+    console.log(`sw: handleSameOriginRequest: getting data from cache for ${requestUrl}`);
+    const response = await caches.match(request);
+
+    if (response) {
+        console.log(`sw: handleSameOriginRequest: found data from cache for ${requestUrl}`);
 
         return response;
-    } catch (err) {
-        const response = await caches.match(request);
-
-        return response;
+    } else {
+        console.log(`sw: handleSameOriginRequest: no data found from cache for ${requestUrl}`);
+        return new Response('no-match');
     }
 }
